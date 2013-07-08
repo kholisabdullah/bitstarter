991965edec3f308918f9edc050062adc31ff162b
Kholis Abdullah <kholis.abdullah@gmail.com>
2013-07-09 05:15:05 +0700

    Read index.html to response

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!);
+    var content = fs.readFileSync("./index.html");
+    var buffer = new Buffer(content, "utf-8");
+    response.send(buffer.toString("utf-8", 0, buffer.length));
 });
 
 var port = process.env.PORT || 5000;
